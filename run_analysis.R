4c31a994fe009d49617c0636df1b7fb5014d0028
gitsduran1986 <49067233+gitsduran1986@users.noreply.github.com>
2020-06-10 11:44:26 -0400

    code to select columns added

--- run_analysis.R
+++ run_analysis.R
@@ -1,4 +1,5 @@
 library(readr)
+library(dplyr)
 
 #aquire data
 test.X <- read.table("./UCI HAR Dataset/test/X_test.txt")
@@ -7,8 +8,37 @@ train.Y <- read.table("./UCI HAR Dataset/train/Y_train.txt")
 test.Y <- read.table("./UCI HAR Dataset/test/Y_test.txt")
 
 #merge data into one df
-test <- cbind(test.X,test.Y)
-train <- cbind(train.X,train.Y)
-df <- rbind(test,train)
+df <- rbind(test.X,train.X)
+df.Y <- rbind(test.Y,train.Y)
+names(df.Y) <- c("Activity")
+
+#pull in list of features
+features <- read.table("./UCI HAR Dataset/features.txt")
+
+#define helpful function to say if string contains values in meanstd
+mean.std <- function(string) {
+  meanstd <- c("mean()","std()")
+  x <- FALSE
+  for (i in meanstd) {
+    if (grepl(i,string)){
+      if (!grepl("meanFreq()",string)){
+        x <- TRUE
+        break
+      }
+    }
+  }
+  x
+}
+
+#create mask for features I want to include
+mask <- c()
+for (i in features$V2) {
+  mask <- append(mask,mean.std(i))
+}
+
+#select columns with values TRUE in mask
+df <- df[,mask]
+names(df) <- features$V2[mask]
+df <- cbind(df,df.Y)
 
 
